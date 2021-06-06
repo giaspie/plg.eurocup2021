7fd25c5b6f67100c9b20ad33750dbafb13fde329
giaspie <33163972+giaspie@users.noreply.github.com>
2021-06-06 18:14:37 +0100

    Adding Groups

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const groups = {
+    groupA: ["Italy", "Switzerland", "Wales", "Turkey"],
+    groupB: ["Belgium", "Denmark", "Finland", "Russia"],
+    groupC: ["Austria", "Netherlands", "Macedonia FYR", "Ukraine"],
+    groupD: ["Croatia", "Czech-Republic", "England", "Scotland"],
+    groupE: ["Slovakia", "Poland", "Spain", "Sweden"],
+    groupF: ["France", "Germany", "Hungary", "Portugal"]
+}
+let APIcalls = 0;
+let maxAPIcalls = 0;
 const requestOptions = {
     method: 'GET',
     headers: {
@@ -8,55 +18,37 @@ const requestOptions = {
 };
 
 fetch("https://api-football-beta.p.rapidapi.com/teams?league=4&season=2020", requestOptions)
-    .then(response => response.json())
+    .then(response => {
+        APIcalls = 100 - response.headers.get('x-ratelimit-requests-remaining')
+        maxAPIcalls = response.headers.get('x-ratelimit-requests-limit')
+        document.getElementById("APIcalls").textContent = "API calls : " + APIcalls + "/" + maxAPIcalls;
+        return response.json()
+    })
     .then(result => {
         result.response.forEach(element => {
+            let groupNAme = Object.keys(groups).find(key => groups[key].includes(element.team.country));
+            if (groupNAme) {
+                let groupStageElt = document.getElementById(groupNAme);
 
-            let groupStageElt = document.getElementById("groupStage");
+                let teamElt = document.createElement("div");
+                let flagElt = document.createElement("img");
+                let countryElt = document.createElement("div");
 
-            let teamElt = document.createElement("div");
-            let flagElt = document.createElement("img");
-            let countryElt = document.createElement("div");
+                teamElt.classList.add("team");
+                flagElt.classList.add("flag");
+                countryElt.classList.add("country");
 
-            teamElt.classList.add("team");
-            flagElt.classList.add("flag");
-            countryElt.classList.add("country");
+                flagElt.setAttribute("src", element.team.logo)
+                flagElt.setAttribute("alt", element.team.country + "'s Flag")
+                countryElt.textContent = element.team.country;
 
-            flagElt.setAttribute("src", element.team.logo)
-            flagElt.setAttribute("alt", element.team.country+"'s Flag")
-            countryElt.textContent = element.team.country;
-
-            teamElt.appendChild(flagElt);
-            teamElt.appendChild(countryElt);
-            groupStageElt.appendChild(teamElt);
+                teamElt.appendChild(flagElt);
+                teamElt.appendChild(countryElt);
+                groupStageElt.appendChild(teamElt);
+            }
         });
 
         return console.log(result)
     })
     .catch(error => console.log('error', error));
 
-// let groupStageElt = document.getElementById("groupStage");
-
-// let teamElt = document.createElement("div");
-// let flagElt = document.createElement("img");
-// let countryElt = document.createElement("div");
-
-// teamElt.classList.add("team");
-// flagElt.classList.add("flag");
-// countryElt.classList.add("country");
-
-// flagElt.setAttribute("src", "https://media.api-sports.io/football/teams/2.png")
-// flagElt.setAttribute("alt", "France's Flag")
-// countryElt.textContent = "France";
-
-// teamElt.appendChild(flagElt);
-// teamElt.appendChild(countryElt);
-// groupStageElt.appendChild(teamElt);
-
-
-
-
-
-// document
-//     .getElementById("ask-hello")
-//     .addEventListener("click", askHello);
